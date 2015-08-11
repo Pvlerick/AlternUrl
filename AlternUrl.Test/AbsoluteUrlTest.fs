190148a957c393841f9f8101cef819a0122e0370
Philippe <pvlerick@gmail.com>
2015-08-11 19:41:39 +0200

    Ported test project to F# too, will write a CSharp wrapper and tests over it

---
+++ AlternUrl.Test/AbsoluteUrlTest.fs
@@ -0,0 +1,57 @@
+﻿module AlternUrl.Test.AbsoluteUrlTest
+
+open System
+open AlternUrl
+open Xunit
+
+[<Fact>]
+let ``constructor with string`` () =
+    // Fixture setup
+    let sut = new AbsoluteUrl("http://username:password@example.com:8042/over/there/index.dtb?type=animal&name=narwhal#nose")
+    // Exercise system & Verify outcome
+    Assert.Equal("http", sut.Scheme)
+    Assert.Equal("username:password", sut.UserInfo)
+    Assert.Equal("example.com", sut.Host)
+    Assert.Equal(8042, sut.Port)
+    Assert.Equal("/over/there/index.dtb", sut.Path)
+    Assert.Equal("type=animal&name=narwhal", sut.Query)
+    Assert.Equal("nose", sut.Fragment)
+    Assert.Equal("index", sut.FileName)
+    Assert.Equal(".dtb", sut.Extension)
+    // Teardown
+
+[<Fact>]
+let ``constructor with uri`` () =
+    // Fixture setup
+    let uri = new Uri("http://username:password@example.com:8042/over/there/index.dtb?type=animal&name=narwhal#nose")
+    let sut = new AbsoluteUrl(uri)
+    // Exercise system & Verify outcome
+    Assert.Equal("http", sut.Scheme)
+    Assert.Equal("username:password", sut.UserInfo)
+    Assert.Equal("example.com", sut.Host)
+    Assert.Equal(8042, sut.Port)
+    Assert.Equal("/over/there/index.dtb", sut.Path)
+    Assert.Equal("type=animal&name=narwhal", sut.Query)
+    Assert.Equal("nose", sut.Fragment)
+    Assert.Equal("index", sut.FileName)
+    Assert.Equal(".dtb", sut.Extension)
+    // Teardown
+
+[<Fact>]
+let ``constructor with null throws``() =
+    // Fixture setup
+    // Exercise system & Verify outcome
+    Assert.Throws<ArgumentNullException>(fun () -> new AbsoluteUrl(null) |> ignore)
+    // Teardown
+
+[<Theory>]
+[<InlineData("")>]
+[<InlineData("foo")>]
+[<InlineData("ftp://hello")>] //Invalid scheme
+[<InlineData("htttp://hello")>]
+[<InlineData("httpss://www.google.com")>]
+let ``constructor with invalid url throws``(urlText:string) =
+    // Fixture setup
+    // Exercise system & Verify outcome
+    Assert.Throws<ArgumentException>(fun () -> new AbsoluteUrl(urlText) |> ignore)
+    // Teardown
--- AlternUrl/AbsoluteUrl.fs
+++ AlternUrl/AbsoluteUrl.fs
@@ -4,7 +4,7 @@ open System
 open System.Runtime.InteropServices
 open System.Text.RegularExpressions
 open System.Web
-//
+
 //[<AbstractClass>]
 //type Url() =
 //    abstract member FileName : string with get
@@ -12,15 +12,26 @@ open System.Web
 //    abstract member Extension : string with get
 //    abstract member HasExtension : bool with get
 
+/// <summary>
+/// A class that respresents an absolute url.
+/// E.g: "http://www.github.com/"
+/// </summary>
 type AbsoluteUrl private (scheme: string, userInfo: string, host: string, port: int, path: string, query: string, fragment: string) = 
     //inherit Url()
     do 
         if scheme = null then raise (ArgumentNullException("scheme"))
 
-    new(url: string, [<Optional;DefaultParameterValue(true)>]encoded: bool) =
+    /// <summary>
+    /// Creates a new instance of the AbsoluteUrl class, using the given <cref="System.String" /> that must be a valid URL
+    /// </summary>
+    /// <param name="url">The string representation of the URL to be parsed to create the object.</param>
+    /// <param name="encoded">Indicates if the given string is encoded or not. Default is true.</param>
+    /// <exception cref="System.ArgumentNullException">Thrown when the input string is null.</exception>
+    /// <exception cref="System.ArgumentException">Thrown when the input string is not a valid URL.</exception>
+    new(url: string, ?encoded: bool) =
         if url = null then raise (ArgumentNullException("url"))
 
-        let url0 = if encoded then HttpUtility.UrlEncode(url) else url
+        let url0 = defaultArg encoded true
 
         //Parsing with regex from RFC 3986, Appendix B. - http://www.ietf.org/rfc/rfc3986.txt
         let match0 = Regex.Match(url, "^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?")
@@ -28,7 +39,7 @@ type AbsoluteUrl private (scheme: string, userInfo: string, host: string, port: 
 
         if not match0.Success || scheme = "" then raise (ArgumentException("url is not valid", "url"))
 
-        if not (Regex.IsMatch(scheme, "https*", RegexOptions.IgnoreCase)) then raise (ArgumentException("Scheme has to be http or https for an absolute URL"))
+        if not (Regex.IsMatch(scheme, "^https?$", RegexOptions.IgnoreCase)) then raise (ArgumentException("Scheme has to be http or https for an absolute URL"))
 
         let authority = match0.Groups.[4].Value
         let path = "/" + match0.Groups.[5].Value.TrimStart('/')
@@ -38,15 +49,18 @@ type AbsoluteUrl private (scheme: string, userInfo: string, host: string, port: 
         //Userinfo, host and port are found with further parsing of the authority
         let match1 = Regex.Match(authority, @"(([^@]+)@)?([^:]+)(:(\d+))?")
         let userInfo = match1.Groups.[2].Value
-        let host = match1.Groups.[3].Value.ToLowerInvariant()
+        let host = match1.Groups.[3].Value
 
         let port =
             if not (String.IsNullOrEmpty(match1.Groups.[5].Value)) then Convert.ToInt32(match1.Groups.[5].Value)
             else
-                if scheme = "http" then 80 else 443 //Default port for http(s) scheme
+                if scheme.ToLowerInvariant() = "http" then 80 else 443 //Default port for http(s) schemes
 
         AbsoluteUrl(scheme, userInfo, host, port, path, query, fragment)
 
+    /// <summary>
+    /// Creates a new instance of the AbsoluteUrl class, using the given <cref="System.Uri" />
+    /// </summary>
     new(uri: Uri) =
         if uri = null then raise (ArgumentNullException("uri"))
         AbsoluteUrl(uri.ToString(), true)
@@ -62,3 +76,15 @@ type AbsoluteUrl private (scheme: string, userInfo: string, host: string, port: 
     member this.HasFileName = this.FileName <> ""
     member this.Extension = System.IO.Path.GetExtension(this.Path)
     member this.HasExtension = this.Extension <> ""
+
+    member this.IsDomainAnIPAddress = Regex.IsMatch(this.Host, @"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+    
+    member this.TopLevelDomain =
+        if this.IsDomainAnIPAddress then raise (NotSupportedException("This AbsoluteUrl's domain is an IP address so there is no top level domain. Use the IsDomainAnIpAddress property to check before calling TopLevelDomain."))
+        else this.Host.Substring(this.Host.LastIndexOf(".") + 1);
+
+    member this.SecondLevelDomain = 
+        if this.IsDomainAnIPAddress then raise (NotSupportedException("This AbsoluteUrl's domain is an IP address so there is no second level domain. Use the IsDomainAnIpAddress property to check before calling SecondLevelDomain."))
+        else
+            let hostWithoutTopLevelDomain = this.Host.Substring(0, this.Host.LastIndexOf("."))
+            this.Host.Substring(hostWithoutTopLevelDomain.LastIndexOf(".") + 1)
